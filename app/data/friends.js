fb07fd2aca46943739bfa000f1de94bf11732f2f
Javier Leon <javierleon01@yahoo.com>
2018-04-23 18:45:04 -0700

    late update

---
+++ app/data/friends.js
@@ -0,0 +1,72 @@
+let friends = [
+{
+  "name":"Kim Kardashian",
+  "photo":"https://static.comicvine.com/uploads/original/3/31265/1550715-chuck_norris_2.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      3
+    ] 
+},
+{
+  "name":"Lil Pump",
+  "photo":"http://cdn-static.denofgeek.com/sites/denofgeek/files/styles/main_wide/public/2017/02/header_1.jpg?itok=bqQ00vcZ",
+  "scores":[
+      3,
+      2,
+      5,
+      1,
+      2
+    ]
+},
+{
+  "name":"Elton John",
+  "photo":"https://www.thefamouspeople.com/profiles/images/jean-claude-van-damme-3.jpg",
+  "scores":[
+      2,
+      3,
+      4,
+      4,
+      3
+    ]
+},
+{
+  "name":"Trippie Redd",
+  "photo":"https://timedotcom.files.wordpress.com/2015/03/bruce-willis-movies-01.jpg?quality=85&w=838",
+  "scores":[
+      5,
+      5,
+      5,
+      5,
+      4
+    ]
+},
+{
+  "name":"iCarly",
+  "photo":"https://www.thesun.co.uk/wp-content/uploads/2017/06/nintchdbpict000190860133.jpg?strip=all&w=960",
+  "scores":[
+      2,
+      3,
+      4,
+      5,
+      1
+    ]
+},
+{
+  "name":"Ellen Degenerous",
+  "photo":"http://www.slate.com/content/dam/slate/blogs/moneybox/2013/11/06/silk_road_2_0_dread_pirate_roberts_is_resurrected/1383765045.png.CROP.promo-mediumlarge.png",
+  "scores":[
+      4,
+      2,
+      3,
+      1,
+      4
+    ]
+}
+];
+
+module.exports = friends;
+
+
